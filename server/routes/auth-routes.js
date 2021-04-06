2cbfc4a181f5d867fc650f93e53e4dc17c31f2ba
douglas86 <douglasmaxton@gmail.com>
2021-04-06 21:26:39 +0200

    Run find on db to see if record exists

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -5,15 +5,30 @@ const router = express.Router();
 
 router.post("/google", (req, res) => {
   let token = req.body.googleInfo.profileObj;
-  console.log(token.googleId);
-  new User({
+  // find data from db using google id
+  User.findOne({
     googleId: token.googleId,
-    imageUrl: token.imageUrl,
-    email: token.email,
-    name: token.name,
-    givenName: token.givenName,
-    familyName: token.familyName,
-  }).save();
+  }).then((currentUser) => {
+    // if used is in db
+    if (currentUser) {
+      console.log("user is: ", currentUser);
+      // if user not in db
+    } else {
+      new User({
+        googleId: token.googleId,
+        imageUrl: token.imageUrl,
+        email: token.email,
+        name: token.name,
+        givenName: token.givenName,
+        familyName: token.familyName,
+      })
+        .save()
+		// once saved to db
+        .then((newUser) => {
+          console.log("new user created: " + newUser);
+        });
+    }
+  });
 });
 
 export default router;
