629e3f6b20c1451861fd821a51f3d02ab3d6ed7c
douglas86 <douglasmaxton@gmail.com>
2021-04-06 14:21:22 +0200

    Recreated client, server and Readme.md

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.get(`/`, (req, res) => {
+	res.send(`Hello World`)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`)
+})
