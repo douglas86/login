1b5b08d219552acff9878be38869c1bb2bc04e87
douglas86 <douglasmaxton@gmail.com>
2021-04-08 18:43:35 +0200

    Created a new component called Form.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,25 +1,11 @@
-import logo from "../images/logo.svg";
-import "../styling/App.css";
-import Login from "./Google/login.jsx";
+import GoogleBtn from "./Google/login.jsx";
+import LoginForm from "./Login/Form.jsx";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Login />
+    <div>
+      <LoginForm />
+      <GoogleBtn />
     </div>
   );
 };
