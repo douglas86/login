cb000fb29d5e79f8640c4ff945461cd2f6dec090
douglas86 <douglasmaxton@gmail.com>
2021-04-13 09:31:03 +0200

    Post data to db for the register form

--- client/src/components/Register/Submit.jsx
+++ client/src/components/Register/Submit.jsx
@@ -1,30 +1,35 @@
-import { useContext } from 'react';
-import { Context } from './Context';
+import { useContext } from "react";
+import { Context } from "./Context";
+import axios from "axios";
 
 const Submit = () => {
-    const User = useContext(Context);
+  const User = useContext(Context);
 
-    const handleSubmit = () => {
-        console.log(User.data);
-        if (User.data.pName !== User.data.p2Name) {
-            alert('Passwords do not match');
-        } else {
-            alert('Passwords match');
-        }
-    };
+  const handleSubmit = () => {
+    console.log(User.data);
+    if (User.data.pName !== User.data.p2Name) {
+      alert("Passwords do not match");
+    } else {
+      alert("Passwords match");
+      axios
+        .post("http://localhost:5000/user/regular", { user: User.data })
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    }
+  };
 
-    const handleInputChange = (e) => {
-        e.persist();
-        User.setData((i) => ({
-            ...i,
-            [e.target.name]: e.target.value,
-        }));
-    };
+  const handleInputChange = (e) => {
+    e.persist();
+    User.setData((i) => ({
+      ...i,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    return {
-        handleSubmit,
-        handleInputChange,
-    };
+  return {
+    handleSubmit,
+    handleInputChange,
+  };
 };
 
 export default Submit;
