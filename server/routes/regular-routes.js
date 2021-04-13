6034508da6c2af48c438642cc4a66bd032705b16
douglas86 <douglasmaxton@gmail.com>
2021-04-13 11:39:50 +0200

    Created a catch statement if hash is broken

--- server/routes/regular-routes.js
+++ server/routes/regular-routes.js
@@ -4,11 +4,13 @@ import bcrypt from "bcrypt";
 
 const router = express.Router();
 
+// Register new users to db
 router.post("/regular", (req, res) => {
+  // Dictionary sent to express from react of registered users
   let token = req.body.user;
   const saltRounds = 10;
 
-  bcrypt.hash(token.pName, saltRounds, (err, hash) => {
+  bcrypt.hash(token.pName, saltRounds, (hash) => {
     new User({
       firstName: token.fName,
       lastName: token.lName,
@@ -18,7 +20,8 @@ router.post("/regular", (req, res) => {
       .save()
       .then((newUser) => {
         console.log("New user created: " + newUser);
-      });
+      })
+      .catch((err) => console.log(err));
   });
 });
 
