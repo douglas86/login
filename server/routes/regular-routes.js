07c3e15840d45ee62f8bf52c17687055084c931a
douglas86 <douglasmaxton@gmail.com>
2021-04-13 11:34:37 +0200

    Encyted password for db using bcrypt

--- server/routes/regular-routes.js
+++ server/routes/regular-routes.js
@@ -1,20 +1,25 @@
 import express from "express";
 import User from "../model/regular-users";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 router.post("/regular", (req, res) => {
   let token = req.body.user;
-  new User({
-    firstName: token.fName,
-    lastName: token.lName,
-    username: token.uName,
-    password: token.pName,
-  })
-    .save()
-    .then((newUser) => {
-      console.log("new user created: " + newUser);
-    });
+  const saltRounds = 10;
+
+  bcrypt.hash(token.pName, saltRounds, (err, hash) => {
+    new User({
+      firstName: token.fName,
+      lastName: token.lName,
+      username: token.uName,
+      password: hash,
+    })
+      .save()
+      .then((newUser) => {
+        console.log("New user created: " + newUser);
+      });
+  });
 });
 
 export default router;
